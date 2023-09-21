42338b5f704b59532182c9e7ddecc1c0303240cd
Viet <vanvietnc99@gmail.com>
2023-09-21 13:29:50 +0700

    vanviet

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -14,6 +14,7 @@ import Payment from "./payment";
 import ProductPackage from "./productPackage";
 import PaymentInfo from "./paymentInfo";
 import priceFormat from "../config/priceFormat";
+import { useTranslation } from "react-i18next";
 
 const Profile = ({ setdisable }) => {
     const counter = useSelector((state) => state.counter);
@@ -22,6 +23,7 @@ const Profile = ({ setdisable }) => {
     const [dataPass, setDataPass] = useState({ oldpass: null, newPass: null, renewPass: null })
     const [error, setError] = useState({ type: "error", error: "" });
     const [open, setOpen] = useState(false);
+    const { t } = useTranslation();
 
     const handleClose = (event, reason) => {
         if (reason === "clickaway") {
@@ -115,7 +117,7 @@ const Profile = ({ setdisable }) => {
                         <div className="flex flex-col justify-center items-center col-span-1 border !border-[#999] p-4 rounded-md">
                             <img className="w-[190px] rounded-full" src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="" />
                             <p className="text-xl mt-4">{user?.username}</p>
-                            {!openChange ? <button className="text-[16px] text-blue-600 mt-4" onClick={() => { setOpenChange(true) }}>Đổi mật khẩu</button>
+                            {!openChange ? <button className="text-[16px] text-blue-600 mt-4" onClick={() => { setOpenChange(true) }}>{t('Đổi mật khẩu')}</button>
                                 : null}
                         </div>
                         <div className="col-span-2 border !border-[#999] h-fit  p-4 text-[16px] text-[#212529] rounded-md">
@@ -123,43 +125,43 @@ const Profile = ({ setdisable }) => {
                                 {dataProfile.map(value => {
                                     return (
                                         <div key={value.title} className="flex border-b border-b-[#c2c1c1] py-3">
-                                            <div className="w-[200px] mr-[50px]">{value.title}</div>
-                                            <div className="w-full text-[#212529BF]">{value.title == "Số dư" || value.title == "Số tiền đã xử dụng" ?`${ priceFormat(value.value / Number(percent_number))} C` : priceFormat(value.value)}</div>
+                                            <div className="w-[200px] mr-[50px]">{t(value.title)}</div>
+                                            <div className="w-full text-[#212529BF]">{value.title === "Số dư" || value.title === "Số tiền đã xử dụng" ?`${ priceFormat(value.value / Number(percent_number))} C` : priceFormat(value.value)}</div>
                                         </div>
                                     )
                                 })}
                             </div> : <div>
-                                <h1 className="flex items-center justify-start mb-4"> <TbArrowBackUp onClick={() => { setOpenChange(false) }} className="mr-2 cursor-pointer hover:scale-125 hover:text-blue-600 text-xl duration-300" /> Đổi mật khẩu</h1>
+                                <h1 className="flex items-center justify-start mb-4"> <TbArrowBackUp onClick={() => { setOpenChange(false) }} className="mr-2 cursor-pointer hover:scale-125 hover:text-blue-600 text-xl duration-300" /> {t('Đổi mật khẩu')}</h1>
                                 <div>
-                                    <p className='text-base mb-2 '>Mật khẩu hiện tại:</p>
-                                    <input type="email" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.oldpass} onChange={(e) => { handleChangeValue(e, "oldpass") }} placeholder='Mật khẩu' />
+                                    <p className='text-base mb-2 '>{t('Mật khẩu hiện tại:')}</p>
+                                    <input type="email" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.oldpass} onChange={(e) => { handleChangeValue(e, "oldpass") }} placeholder={t('password')} />
                                 </div>
                                 <div>
-                                    <p className='text-base mb-1 mt-1 '>Mật khẩu mới:</p>
-                                    <input type="text" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.newPass} onChange={(e) => { handleChangeValue(e, "newPass") }} placeholder='Mật khẩu mới' />
+                                    <p className='text-base mb-1 mt-1 '>{t('Mật khẩu mới:')}</p>
+                                    <input type="text" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.newPass} onChange={(e) => { handleChangeValue(e, "newPass") }} placeholder={t('Mật khẩu mới:')} />
                                 </div>
                                 <div>
-                                    <p className='text-base mb-1 mt-1 '>Xác nhận:</p>
-                                    <input type="text" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.renewPass} onChange={(e) => { handleChangeValue(e, "renewPass") }} placeholder='Xác nhận ' />
+                                    <p className='text-base mb-1 mt-1 '>{t("Xác nhận:")}</p>
+                                    <input type="text" className='w-full focus:border-1 placeholder:italic placeholder:text-sm focus:border-blue-800 mb-2 p-1 ' value={dataPass.renewPass} onChange={(e) => { handleChangeValue(e, "renewPass") }} placeholder={t("Xác nhận:")} />
                                 </div>
                                 <div className="flex justify-center">
-                                    <button onClick={handleChangePassword} className="text-base rounded-md text-white px-20 mt-4 py-2 bg-[#004a99f5] hover:bg-[#2c5f96f5]">Đổi mật khẩu</button>
+                                    <button onClick={handleChangePassword} className="text-base rounded-md text-white px-20 mt-4 py-2 bg-[#004a99f5] hover:bg-[#2c5f96f5]">{t('Đổi mật khẩu')}</button>
                                 </div>
                             </div>}
 
                         </div>
                     </div>
 
-                    <h1 className="text-xl text-[#000] font-medium mt-[100px]  mb-3">Mua bit</h1>
+                    <h1 className="text-xl text-[#000] font-medium mt-[100px]  mb-3">{t('Mua coin')}</h1>
                     {/* border !border-[#999] */}
                     <div className=" h-fit mb-5 border !border-[#f0f0f0]">
                         <Payment setError={setError} />
                     </div>
-                    <h1 className="text-xl text-[#000] font-medium mt-[100px] mb-3">Mua Gói</h1>
+                    <h1 className="text-xl text-[#000] font-medium mt-[100px] mb-3">{t('Mua Gói')}</h1>
                     <div className=" h-fit mb-5 border !border-[#f0f0f0]">
                         <ProductPackage setError={setError} setOpen={setOpen} />
                     </div>
-                    <h1 className="text-xl text-[#000] font-medium mt-[100px] mb-3">Thông tin thanh toán</h1>
+                    <h1 className="text-xl text-[#000] font-medium mt-[100px] mb-3">{t('Thông tin thanh toán')}</h1>
                     <div className=" h-[480px] mb-5 border !border-[#f0f0f0]  overflow-hidden overflow-y-auto p-0.5">
                         <PaymentInfo />
                     </div>
